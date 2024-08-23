053c7bea0e6a5bb18d4b55f0b7b549f8011daf2f
Rii <bryan.donmarc@gmail.com>
2024-08-23 14:17:06 +0800

    ✨ add eslint stylistic

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  typescript: {
+    typeCheck: 'build',
+  },
+  srcDir: 'src/',
+  modules: ['@unocss/nuxt', '@nuxt/eslint'],
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
 })
