9d120da54e06cb58bf3b3675c5d093f0450f3e4a
Rii <bryan.donmarc@gmail.com>
2024-09-15 16:24:51 +0800

    ⚗️ test nuxt studio component

---
+++ src/components/content/Test.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+interface TestProps {
+  id: string
+}
+
+withDefaults(defineProps<TestProps>(), {
+  id: 'Test ID',
+})
+</script>
+
+<template>
+  <div>
+    <h1>{{ id }}</h1>
+  </div>
+</template>
